ad77fed2d63404888cd692d972d4dfa3548f1f8d
ALMO1115 <103348573+ALMO1115@users.noreply.github.com>
2022-05-21 00:10:14 -0700

    completed exercises with alerts, strings, expressions, etc

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,18 @@
+document.write('Hello, World!')
+var A = "This is a string";
+var B = "Ugga Mugga";
+document.write(A); //Displays text on screen
+window.alert(B) //Results in an alert/pop-up window with text from variable B (which is Ugga Mugga)
+document.write("Lyn said,\"Bean, I need 3\\4 cup sugar,\" to Rone."); //Practicing using the escape character
+document.write("Dawson is 11th , " + "James is 8th, " + "Pamella is 2nd."); //Practicing concatenating
+var C = "Hockey & " + "Football"
+document.write(C)
+
+var Family = "The Millers", Dad = "John", Mom = "Lee", Son = "Dawson", Son2 = "James", Daughter = "Lyn";
+document.write(Son2)
+document.write(3+3)
+
+function My_First_Function () { //Defining a function and naming it
+    var str = "This is the button text!"; //Defining a variable and giving it a string value
+    document.getElementById("Button_Text").innerHTML  = str; //Putting the value of the variable into the HTML elementFromPoint with the "Button_Text" id
+}
