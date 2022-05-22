c586a2e3b0053c10a21acc3f60c61a793260060c
ALMO1115 <103348573+ALMO1115@users.noreply.github.com>
2022-05-22 15:28:18 -0700

    updated with my commenting code

--- Basic JavaScript Projects/Project3_math_operators/JS/main.js
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -1,49 +1,49 @@
-function addition_Function() {
+function addition_Function() { //Adding
     var addition = 2 + 2;
-    document.getElementById("Add").innerHTML = "" + addition;
+    document.getElementById("Add").innerHTML = "" + addition; //"Add" is id used in HTML along with name of function above
 }
 
-function subtraction_Function() {
+function subtraction_Function() { //Subtracting
     var subtraction = 5 - 2;
     document.getElementById("Subtract").innerHTML="" + subtraction;
 }
 
-function multiplication_Function() {
+function multiplication_Function() { //Multiplication
     var multiplication = 6 * 8;
     document.getElementById("Multiply").innerHTML="" + multiplication;
 }
 
-function division_Function() {
+function division_Function() { //Division
     var division = 48/6;
     document.getElementById("Divide").innerHTML="" + division;
 }
 
-function more_Math() {
-    var simple_Math = (1+2)*10/2-5;
+function more_Math() { //Multiple operator math in one problem
+    var simple_Math = (1+2)*10/2-5; //remember PEMDAS for oder of operations
     document.getElementById("Math").innerHTML="1 plus 2, multiplied by 10, divided in half and then subtracted by 5 equals" + simple_Math;
 }
 
-function modulus_Operator() {
-    var remainder = 25 % 6;
+function modulus_Operator() { //Obtaining just the remainder from a divion problem
+    var remainder = 25 % 6;                       //Below is what will display when "Remainder" in HTML is clicked on (along with the remainder variable)
     document.getElementById("Remainder").innerHTML="When you divide 25 by 6 you have a remainder of: " + remainder;
 }
 
-function negation_Operator() {
+function negation_Operator() { //Displaying the negative of the variable
     var x = 5
     document.getElementById("Negative").innerHTML = - x
 }
 
-var increment = 10;
+var increment = 10; //Going up one from variable amount
 increment ++
 document.write(increment);
 
-var decrement = 16;
+var decrement = 16; //Going down one from variable digit
 decrement --;
 document.write(decrement);
 
-document.write(Math.random()*8);
+document.write(Math.random()*8); //Displaying a random number - also multiplied by 8
 
-//Math object method--this one rounds to the nearest ten
+//Math object method---this one rounds to the nearest ten
 function roundup_Operator() {
     var up = 23.5
     document.getElementById("roundup").innerHTML = Math.round(up)
